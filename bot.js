ff89cc125090f01168cf09716f00b26444c80ab4
kedckemirhan <57150139+6mirha7@users.noreply.github.com>
2020-12-24 10:11:32 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableEveryone: true });
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+client.on('message', message => {
+if (message.content.toLowerCase() === prefix + 'Komut') {
+  message.member.send("```diff\n-Komut Yazısı\n```")
+}
+})
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.on("message", msg => {
+  var dm = client.users.cache.get("Kullanıcı İd'iniz"); 
+  if (msg.author.id === client.user.id) return
+  if (msg.channel.type === "dm") {
+  const DMEmbed = new Discord.MessageEmbed()
+  .setTitle('Bir Mesaj Geldi')
+  .setAuthor(msg.author.tag, msg.author.avatarURL({size: 1024}))
+  .setDescription(`${msg.content}`)
+  .setThumbnail(msg.author.avatarURL({size: 1024}))
+  dm.send(DMEmbed)}
+  if (msg.channel.bot) return});
+  
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.login(ayarlar.token);
